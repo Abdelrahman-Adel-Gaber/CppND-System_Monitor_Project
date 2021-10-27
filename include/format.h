7f2bfafa31e36d4742450c7ebd096d474ededab7
Abdelrahman-Adel-Gaber <91066958+Abdelrahman-Adel-Gaber@users.noreply.github.com>
2021-10-27 10:27:56 +0200

    Add files via upload

---
+++ include/format.h
@@ -0,0 +1,11 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+
+#include <string>
+
+namespace Format {
+std::string ElapsedTime(long int times);  // TODO: See src/format.cpp
+std::string Pad(std::string s, char c);// namespace Format
+};                                    
+
+#endif
